fb637d6fcc025dd54857c97346fbc3a373461c3e
Ryan358 <ryan61515@gmail.com>
2022-02-01 16:38:48 -0800

    successful test of motors

--- keyboard_test.py
+++ keyboard_test.py
@@ -1,7 +1,28 @@
+from adafruit_motorkit import MotorKit
 import keyboard
-from gpiozero import LED
+import time
 
-white = LED(27)
+kit = MotorKit()
 
-while keyboard.is_pressed('space'):
-    white.on
+def forward():
+    kit.motor1.throttle =  .30
+    kit.motor2.throttle =  .30
+    kit.motor3.throttle =  .30
+    kit.motor4.throttle =  .30
+    
+def coast():
+    kit.motor1.throttle =  None
+    kit.motor2.throttle =  None
+    kit.motor3.throttle =  None
+    kit.motor4.throttle =  None
+
+
+while True:
+    # Wait for the next event.
+    event = keyboard.read_event()
+    if event.event_type == keyboard.KEY_DOWN and event.name == 'space':
+        print('space was pressed')
+        forward()
+    else:
+        coast()
+        
