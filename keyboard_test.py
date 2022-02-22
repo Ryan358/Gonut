137f5538f51e0957f0f8ced2f3b4d3b50f80307c
Ryan358 <69889842+Ryan358@users.noreply.github.com>
2022-02-22 11:12:12 -0800

    changed keyboard test to use new motor drivers.

--- keyboard_test.py
+++ keyboard_test.py
@@ -1,28 +1,41 @@
-from adafruit_motorkit import MotorKit
 import keyboard
+from __future__ import print_function
 import time
+from front_motor_drivers import front_motors, MAX_SPEED
 
-kit = MotorKit()
+
+class DriverFault(Exception):
+    def __init__(self, driver_num):
+        self.driver_num = driver_num
+
+
+def raiseIfFault():
+    if front_motors.motor1.getFault():
+        raise DriverFault(1)
+    if front_motors.motor2.getFault():
+        raise DriverFault(2)
+
 
 def forward():
-    kit.motor1.throttle =  .30
-    kit.motor2.throttle =  .30
-    kit.motor3.throttle =  .30
-    kit.motor4.throttle =  .30
-    
+    front_motors.enable()
+    front_motors.motor2.setSpeed(MAX_SPEED)
+    raiseIfFault()
+    print('Motor running')
+    time.sleep(0.002)
+
+
 def coast():
-    kit.motor1.throttle =  None
-    kit.motor2.throttle =  None
-    kit.motor3.throttle =  None
-    kit.motor4.throttle =  None
+    front_motors.disable()
 
 
 while True:
     # Wait for the next event.
     event = keyboard.read_event()
-    if event.event_type == keyboard.KEY_DOWN and event.name == 'space':
-        print('space was pressed')
-        forward()
-    else:
-        coast()
-        
+    try:
+        if event.event_type == keyboard.KEY_DOWN and event.name == 'w':
+            print('space was pressed')
+            forward()
+        else:
+            coast()
+    except DriverFault as e:
+        print("Driver %s fault!" % e.driver_num)
